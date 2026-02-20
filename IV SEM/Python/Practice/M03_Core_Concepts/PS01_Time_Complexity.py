5a1b713eab7bad6b5ab44fd2643073f61cb7a3b0
CHARVITHA18 <chcharvitha68@gmail.com>
2026-02-20 09:37:31 +0530

    mes

--- IV SEM/Python/Practice/M03_Core_Concepts/PS01_Time_Complexity.py
+++ IV SEM/Python/Practice/M03_Core_Concepts/PS01_Time_Complexity.py
@@ -3,6 +3,6 @@ def Linear_Search(elements,target):
         if target == elements[i]:
             return i
     return -1
-print(Linear_Search([12,45,78,69,32],12))
-print(Linear_Search([12,45,78,69,32],78))
-print(Linear_Search([12,45,78,69,32],32))
+print(Linear_Search([12,45,78,69,32],12)) # best case
+print(Linear_Search([12,45,78,69,32],78))#avg case
+print(Linear_Search([12,45,78,69,32],32))#worst case
